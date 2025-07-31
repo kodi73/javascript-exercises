958bb842283ab99d9a0cbd1b542d09dffd702709
kodi73 <aditya.kumar.73@outlook.com>
2025-07-31 18:01:46 +0530

    Completed Exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const cleanedString = a.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString===reversedString;
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+        a = parseInt(a, 10);
+    if (a < 0)
+    {
+        return "OOPS";
+    }
 
+    else if (a === 0)
+    {
+        return 0;
+    }
+
+    else if (a === 1)
+    {
+        return 1;
+    }
+
+    else
+    {
+        return fibonacci(a - 1) + fibonacci(a - 2);
+    }
 };
 
 // Do not edit below this line
